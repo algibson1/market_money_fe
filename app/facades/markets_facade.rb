812e01689db539611133447f2727d59d1e399b0b
Artemy Gibson <artemygibson@gmail.com>
2023-09-09 22:01:36 -0700

    Refactor, add helper methods

--- app/facades/markets_facade.rb
+++ app/facades/markets_facade.rb
@@ -1,19 +1,33 @@
 class MarketsFacade 
+  def initialize
+    @markets_by_id = {}
+  end
+
   def all_markets 
-    response = service.all_markets
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    parsed[:data].map do |data| 
-      Market.new(data)
-    end
+    @markets ||= parse(get_markets).map { |data| Market.new(data) }
   end
 
   def find_market(id)
-    response = service.find_market(id)
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    Market.new(parsed[:data])
+    @markets_by_id[id] ||= Market.new(parse(get_market(id)))
+    # better to use all_markets.find { |market| market.id == id } ?
+    # Remove find_market method from service?
   end
 
+  private
+  
   def service
     @_service ||= MarketMoneyService.new
+  end
+  
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)[:data]
+  end
+
+  def get_markets
+    service.all_markets
+  end
+
+  def get_market(id)
+    service.find_market(id)
   end
 end
