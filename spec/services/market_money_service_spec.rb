01afdfd4b3e9bc665e4872cb63023b83afd2520a
Artemy Gibson <artemygibson@gmail.com>
2023-09-10 09:28:49 -0700

    Add test for vendors_for(market) endpoint

--- spec/services/market_money_service_spec.rb
+++ spec/services/market_money_service_spec.rb
@@ -36,4 +36,20 @@ RSpec.describe MarketMoneyService, :vcr do
     expect(market_hash[:attributes]).to have_key(:state)
     expect(market_hash[:attributes]).to have_key(:zip)
   end
+
+  it "returns vendors for a given market" do
+    service = MarketMoneyService.new
+    response = service.vendors_for("322458")
+    vendor_data = JSON.parse(response.body, symbolize_names: true)
+
+    vendor_data[:data].each do |vendor_hash|
+      expect(vendor_hash).to have_key(:id)
+      expect(vendor_hash).to have_key(:attributes)
+      expect(vendor_hash[:attributes]).to have_key(:name)
+      expect(vendor_hash[:attributes]).to have_key(:description)
+      expect(vendor_hash[:attributes]).to have_key(:contact_name)
+      expect(vendor_hash[:attributes]).to have_key(:contact_phone)
+      expect(vendor_hash[:attributes]).to have_key(:credit_accepted)
+    end
+  end
 end
