a6924bd0430a95a376a7cd5654b5ef7825c29623
Artemy Gibson <artemygibson@gmail.com>
2023-09-10 11:03:25 -0700

    Add find_vendor method and get_vendor helper method

--- app/facades/vendors_facade.rb
+++ app/facades/vendors_facade.rb
@@ -1,10 +1,15 @@
 class VendorsFacade
   def initialize 
     @vendors_by_market = {}
+    @vendors_by_id = {}
   end
 
   def vendors_for(id)
     @vendors_by_market[id] ||= parse(get_market_vendors(id)).map { |data| Vendor.new(data) }
+  end
+
+  def find_vendor(id)
+    @vendors_by_id[id] ||= Vendor.new(parse(get_vendor(id)))
   end
 
   private
@@ -19,5 +24,9 @@ class VendorsFacade
 
   def get_market_vendors(id)
     service.vendors_for(id)
+  end
+
+  def get_vendor(id)
+    service.find_vendor(id)
   end
 end
